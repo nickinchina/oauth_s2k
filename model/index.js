e4e8edbf77e650122b2857dd2c3de518250dc929
luckylz2git <8618993@qq.com>
2018-10-01 21:52:50 +0800

    model index

--- model/index.js
+++ model/index.js
@@ -11,79 +11,101 @@ var sql = require('./sql.js');
 
 //Modified by Lucky, on Oct.1,2018
 module.exports.getAccessToken = function(bearerToken) {
-  var params = [
-	{name:'token',type:'NVarChar',length:500,value:bearerToken},
-	{name:'type',type:'TinyInt',value:0}
-  ];
-  sql.query('hq.sp_oauth_get_token',params) //??? how to write setParams ???
-    .then(function(result) {
-      var token = result.rows[0];
-
-      return {
-        accessToken: token.access_token,
-        client: {id: token.client_id},
-        expires: token.expires,
-        user: {id: token.userId}, // could be any object
-      };
-    });
-};
-
-/**
- * Get client.
- */
-
-module.exports.getClient = function *(clientId, clientSecret) {
-  return sql.query('SELECT client_id, client_secret, redirect_uri FROM oauth_clients WHERE client_id = $1 AND client_secret = $2', [clientId, clientSecret])
-    .then(function(result) {
-      var oAuthClient = result.rows[0];
-
-      if (!oAuthClient) {
-        return;
-      }
-
-      return {
-        clientId: oAuthClient.client_id,
-        clientSecret: oAuthClient.client_secret,
-        grants: ['password'], // the list of OAuth2 grant types that should be allowed
-      };
-    });
+	var params = [
+		{name:'token',type:'NVarChar',length:500,value:bearerToken},
+		{name:'type',type:'TinyInt',value:0}
+	];
+	sql.query('hq.sp_oauth_get_token',params)
+	.then(function(result) {
+	 	var token = result.rows[0];
+	  	return {
+			accessToken: token.access_token,
+			client: {id: token.client_id},
+			expires: token.expires,
+			user: {id: token.userId}, // could be any object
+	  	};
+	});
 };
 
 /**
  * Get refresh token.
  */
 
-module.exports.getRefreshToken = function *(bearerToken) {
-  return sql.query('SELECT access_token, access_token_expires_on, client_id, refresh_token, refresh_token_expires_on, user_id FROM oauth_tokens WHERE refresh_token = $1', [bearerToken])
-    .then(function(result) {
-      return result.rowCount ? result.rows[0] : false;
-    });
+//Modified by Lucky, on Oct.1,2018
+module.exports.getRefreshToken = function(bearerToken) {
+	var params = [
+		{name:'token',type:'NVarChar',length:500,value:bearerToken},
+		{name:'type',type:'TinyInt',value:1}
+	];
+	sql.query('hq.sp_oauth_get_token',params)
+	.then(function(result) {
+	 	var token = result.rows[0];
+	 	//return result.rowCount ? result.rows[0] : false;
+	  	return token;
+	});
+};
+
+/**
+ * Get client.
+ */
+
+//Modified by Lucky, on Oct.1,2018
+module.exports.getClient = function(clientId, clientSecret) {
+	var params = [
+		{name:'client_id',type:'NVarChar',length:500,value:clientId},
+		{name:'client_secret',type:'NVarChar',length:500,value:clientSecret}
+	];
+	sql.query('hq.sp_oauth_get_client',params)
+	.then(function(result) {
+		var oAuthClient = result.rows[0];
+		if (!oAuthClient) {
+			return;
+	 	}
+	 	return {
+	 		clientId: oAuthClient.client_id,
+	 		clientSecret: oAuthClient.client_secret,
+	 		grants: ['password']
+	 	};
+	});
 };
 
 /*
  * Get user.
  */
 
-module.exports.getUser = function *(username, password) {
-  return sql.query('SELECT id FROM users WHERE username = $1 AND password = $2', [username, password])
-    .then(function(result) {
-      return result.rowCount ? result.rows[0] : false;
-    });
+//Modified by Lucky, on Oct.1,2018
+module.exports.getUser = function(username, password) {
+	sql.sp='';
+	var params = [
+		{name:'username',type:'NVarChar',length:100,value:username},
+		{name:'password',type:'NVarChar',length:100,value:password}
+	];
+	sql.query('hq.sp_oauth_get_user', params)
+	.then(function(result) {
+		var user = result.rows[0];
+	 	//return result.rowCount ? result.rows[0] : false;
+	  	return user;
+	});
 };
 
 /**
  * Save token.
  */
 
-module.exports.saveAccessToken = function *(token, client, user) {
-  return sql.query('INSERT INTO oauth_tokens(access_token, access_token_expires_on, client_id, refresh_token, refresh_token_expires_on, user_id) VALUES ($1, $2, $3, $4)', [
-    token.accessToken,
-    token.accessTokenExpiresOn,
-    client.id,
-    token.refreshToken,
-    token.refreshTokenExpiresOn,
-    user.id
-  ]).then(function(result) {
-    return result.rowCount ? result.rows[0] : false; // TODO return object with client: {id: clientId} and user: {id: userId} defined
-  });
+//Modified by Lucky, on Oct.1,2018
+module.exports.saveAccessToken = function(token, client, user) {
+	var params = [
+		{name:'access_token',type:'NVarChar',length:500,value:token.access_token},
+		{name:'access_token_expires_on',type:'DateTime',value:token.access_token_expires_on},
+		{name:'client_id',type:'NVarChar',length:500,value:client.id},
+		{name:'refresh_token',type:'NVarChar',length:500,value:token.refresh_token},
+		{name:'refresh_token_expires_on',type:'DateTime',value:token.refresh_token_expires_on},
+		{name:'user_id',type:'Int',length:100,value:user.id}
+	];
+	sql.query('hq.sp_oauth_save_token', params)
+	.then(function(result) {
+		var token = result.rows[0];
+		//return result.rowCount ? result.rows[0] : false;
+		return token;
+	});
 };
