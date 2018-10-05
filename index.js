125cf56878f90e3639f9930ae558017ba7a2d291
nickinchina <shoutsoft@hotmail.com>
2018-10-05 03:01:06 +0000

    c9

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ module.exports = function(app, passport){
   });
   
   app.get('/api/v4/user',function(req, res) {
-    console.log('/api/v4/user', req.body);
+    console.log('/api/v4/user', req.params, req.query);
     var user = req.user;
     if (req.session.passport && req.session.passport.user && req.session.passport.user) user = req.session.passport.user;
     var hash = md5.createHash(user.email.toLowerCase());
