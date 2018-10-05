98cfdab61ed73e983e092ed3b0fece128c1567e1
nickinchina <shoutsoft@hotmail.com>
2018-10-05 03:34:13 +0000

    c9

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ module.exports = function(app, passport){
   app.get('/api/v4/user',function(req, res) {
     var user = req.user;
     if (req.session.passport && req.session.passport.user && req.session.passport.user) user = req.session.passport.user;
-    console.log('/api/v4/user', req);
+    console.log('/api/v4/user', req.headers);
     var hash = md5.createHash(user.email.toLowerCase());
     var avatar_url = 'https://secure.gravatar.com/avatar/' + hash;
     avatar_url += '?s=40&r=pg&d=identicon';
