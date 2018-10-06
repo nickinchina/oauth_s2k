4c9ada197106c21d474adb725ed1fc0a5ad08dac
nickinchina <shoutsoft@hotmail.com>
2018-10-06 00:55:33 +0000

    c9

--- index.js
+++ index.js
@@ -86,32 +86,35 @@ module.exports = function(app, passport){
     console.log('/api/v4/userxxxxxxxxx', req.headers.authorization)
     var token = req.headers.authorization;
     if (token) {
-      try {
         token = token.substr('Bearer '.length);
       return model.getAccessToken(token)
       .then(function(t){
-        var user = JSON.parse(t.user) ;
-        var hash = md5.createHash(user.email.toLowerCase());
-        var avatar_url = 'https://secure.gravatar.com/avatar/' + hash;
-        avatar_url += '?s=40&r=pg&d=identicon';
-        var username = (user.account+':'+user.name.replace(' ','_')).toLowerCase();
-        var ouser = {
-          id: user.id, name: user.name, username:username,state:'active',
-          avatar_url:avatar_url,web_url:'',
-          "created_at" : "0000-00-00T00:00:00.000Z",
-          "bio" : null,"location" : null,"skype" : "","linkedin" : "","twitter" : "","website_url" : "","organization" : null,
-          "last_sign_in_at" : "0000-00-00T00:00:00.000Z","confirmed_at" : "0000-00-00T00:00:00.000Z","last_activity_on" : null,
-          email:user.email,"theme_id" : 1,"color_scheme_id" : 1,"projects_limit" : 100000,"current_sign_in_at" : "0000-00-00T00:00:00.000Z",
-          "identities" : [{"provider" : "s2k","extern_uid" : 1}],
-          "can_create_group" : true,"can_create_project" : true,"two_factor_enabled" : false,"external" : false,"shared_runners_minutes_limit": null
+        try {
+          var user = JSON.parse(t.user) ;
+          var hash = md5.createHash(user.email.toLowerCase());
+          var avatar_url = 'https://secure.gravatar.com/avatar/' + hash;
+          avatar_url += '?s=40&r=pg&d=identicon';
+          var username = (user.account+':'+user.name.replace(' ','_')).toLowerCase();
+          var ouser = {
+            id: user.id, name: user.name, username:username,state:'active',
+            avatar_url:avatar_url,web_url:'',
+            "created_at" : "0000-00-00T00:00:00.000Z",
+            "bio" : null,"location" : null,"skype" : "","linkedin" : "","twitter" : "","website_url" : "","organization" : null,
+            "last_sign_in_at" : "0000-00-00T00:00:00.000Z","confirmed_at" : "0000-00-00T00:00:00.000Z","last_activity_on" : null,
+            email:user.email,"theme_id" : 1,"color_scheme_id" : 1,"projects_limit" : 100000,"current_sign_in_at" : "0000-00-00T00:00:00.000Z",
+            "identities" : [{"provider" : "s2k","extern_uid" : 1}],
+            "can_create_group" : true,"can_create_project" : true,"two_factor_enabled" : false,"external" : false,"shared_runners_minutes_limit": null
+          }
+          console.log('/api/v4/user', ouser)
+          res.json(ouser);
+          res.end();
+          
+        } catch (ex){
+          console.log('xxxxxxx',ex)
+          res.json(400, { error: "empty request" });
+          res.end();
         }
-        console.log('/api/v4/user', ouser)
-        res.json(ouser);
-        res.end();
       })
-      } catch (ex){
-        console.log('xxxxxxx',ex)
-      }
       
     } 
     else {
